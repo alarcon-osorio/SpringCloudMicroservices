59073ca4dafeac0c04b96552ec3a058ca8dbc8d4
alarcon_osorio <alarcon_osorio@hotmail.com>
2024-04-02 23:28:13 -0500

    update security

--- mediApp/src/main/java/com/tlaxcala/security/encrypt/JasyptEncrypt.java
+++ mediApp/src/main/java/com/tlaxcala/security/encrypt/JasyptEncrypt.java
@@ -8,7 +8,7 @@ public class JasyptEncrypt {
         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
         textEncryptor.setPassword("tlaxcala");
 
-        String encryptedText = textEncryptor.encrypt("jdbc:postgresql://172.17.0.5:5432/mediapp_2023_1");
+        String encryptedText = textEncryptor.encrypt("jdbc:postgresql://localhost:5432/mediapp_2023_1");
         System.out.println("Encrypted Text: " + encryptedText);
     }
 }
